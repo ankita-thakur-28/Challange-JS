82a14cc49c4c6c6cf28fba9d4ab2e9f2db0bdf55
ankita-thakur-28 <ankitathakurmfp@gmail.com>
2026-01-14 22:01:04 +0900

    functions program

---
+++ day-1/day-infinity/setTimeout.js
@@ -0,0 +1,20 @@
+//program to pass parameter to setTimeout() //
+
+function greet (){
+    console.log('hello people');
+}
+
+setTimeout(greet, 2000);
+console.log('Good Evening');
+
+
+
+// program to pass parameter to function //
+
+function showWords(firstWords, secondWords){
+    console.log(firstWords);
+    console.log(secondWords);
+}
+
+setTimeout(number, 3000, hello, people);
+console.log('today-learning');
